aa54824976c2d4a879f779d6db62db586f81a002
jaiganesh2108 <jaig7335@gmail.com>
2026-05-31 10:40:45 +0530

    feat: models fastapi

--- fastapi/models.py
+++ fastapi/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
-
+#models.py is where we will define our database models. We will create a model for our todo items.
 class Todo(Base):
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True, index=True)
