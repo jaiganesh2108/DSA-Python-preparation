0e8482931b85ac2434a453e4e7d3f0a5300fb1b3
jaiganesh2108 <jaig7335@gmail.com>
2026-06-07 22:09:58 +0530

    feat: readme update

--- fastapi/database.py
+++ fastapi/database.py
@@ -3,8 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
+# database.py is where we will set up our database connection and create a session. We will use SQLAlchemy for this purpose.
+
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base = declarative_base() 
+Base = declarative_base()
