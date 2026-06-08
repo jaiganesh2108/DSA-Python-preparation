766739609896837e8ee205654a58893de49b22b9
jaiganesh2108 <jaig7335@gmail.com>
2026-06-08 14:50:52 +0530

    feat: main.py get_db func

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from schemas import Todo as TodoSchema
-app = FastAPI()
+from sqlalchemy.orm import Session
 
+app = FastAPI()
+#Dependency for DB section
+def get_db():
+    db = SectionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
 #past todo
 @app.post("/todos", response_model=TodoSchema)
+def create(todo: TodoSchema, db: Session = Depends(get_db)):
+    db_todo = Todo()
