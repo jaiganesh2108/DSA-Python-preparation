2b31503d72f5403cbf98336446640eb046a5358e
jaiganesh2108 <jaig7335@gmail.com>
2026-07-09 21:08:47 +0530

    feat: cpp input stream

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << "Enter the numbers of a:" << a << endl << "and b:" << b << endl;
+    return 0; 
+}
