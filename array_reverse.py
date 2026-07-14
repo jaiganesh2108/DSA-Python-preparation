5db00994d3c817da61015192dc8ec36073a10812
jaiganesh2108 <jaig7335@gmail.com>
2026-07-14 21:06:55 +0530

    feat: creaing an another array

--- array_reverse.py
+++ array_reverse.py
@@ -8,9 +8,31 @@ def array_reverse(arr):
     for i in range(n):
         arr[i] = temp[i]
 
+def array_reverse_two(arr1):
+    n = len(arr1)
+    temp = [0] * n
+    for i in range(n):
+        temp[i] = arr1[n-i-1]
+
+    for i in range(n):
+        arr1[i] = temp[i]
+
 if __name__ == "__main__":
-    arr = [11,2,13,4,15]
+    arr = [1,2,1,4,5]
+    arr1 = [11,2,13,4,15]
+
     array_reverse(arr)
     n = len(arr)
     for i in range(n):
         print(arr[i], end =" ")
+    print("\n")
+    array_reverse_two(arr1)
+    n = len(arr1)
+    for i in range(n):
+        print(arr1[i], end =" ")
+
+
+
+
+
+
