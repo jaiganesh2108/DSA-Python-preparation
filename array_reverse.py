724a34d4a47e81c49d350a3be5cf3ff99b9dbb84
jaiganesh2108 <jaig7335@gmail.com>
2026-07-14 15:10:08 +0530

    feat: miner changes

--- array_reverse.py
+++ array_reverse.py
@@ -1,5 +1,6 @@
 def array_reverse(arr):
     n = len(arr)
+    arr.sort()
     temp = [0] * n
     for i in range(n):
         temp[i] = arr[n-i-1]
@@ -7,11 +8,9 @@ def array_reverse(arr):
     for i in range(n):
         arr[i] = temp[i]
 
-
-
 if __name__ == "__main__":
-    arr = [1,2,3,4,5]
+    arr = [11,2,13,4,15]
     array_reverse(arr)
-    
-    for i in range(len(arr)):
+    n = len(arr)
+    for i in range(n):
         print(arr[i], end =" ")
