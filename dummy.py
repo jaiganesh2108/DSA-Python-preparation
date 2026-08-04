48a86817a7bb805305e602ab98b7cc6e1335912a
jaiganesh2108 <jaig7335@gmail.com>
2026-08-04 21:02:47 +0530

    feat: comment

--- dummy.py
+++ dummy.py
@@ -10,13 +10,13 @@ for i in range(1,21):
 for i,j in zip(range(1,21), range(-21,-1)):
     print(i,j)
 
-print("this is the \n new line")
-print("this is the\ttab space")
-print("this is the backslash \\")
+#print("this is the \n new line")
+#print("this is the\ttab space")
+#print("this is the backslash \\")
 
-a = int(23)
-b = float(74.3)
-print(a,b, sep=",")
+#a = int(23)
+#b = float(74.3)
+#print(a,b, sep=",")
 
 i = 0
 while i < 6:
@@ -25,4 +25,8 @@ while i < 6:
         continue
     print(i)
 
-    
+#print('this is shiva\'s code')
+#print(" He says, \"this is my phone\"")
+#print(" this is the backslash: \\")
+#print("It's Hero Time! \n\t - Ben 10")
+
