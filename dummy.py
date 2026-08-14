b731938f9c6e0f374e4180288a596c64de2b6c68
jaiganesh2108 <jaig7335@gmail.com>
2026-08-14 21:37:47 +0530

    feat: function defenition

--- dummy.py
+++ dummy.py
@@ -1,19 +1,8 @@
-x = 50
-print("x =", x, "\t", "type:", type(x))
+def add():
+    a = int(input())
+    b = int(input())
+    return (a+b)
 
-x = 50.0
-print("x =", x, "\t", "type:", type(x))
-
-def add(x: int, y: int) -> float:
-    return x / y
-
-output = add(5, 10)
-print("Output:", output, "\t", "type:", type(output))
-
-name = "jaiganesh"
-print(name[0], name[8])
-
-list = ["jai", 100, "python", "programmer"]
-
-for i in list:
-    print(i)
+sum1 = add()
+print(sum1)
+add()
