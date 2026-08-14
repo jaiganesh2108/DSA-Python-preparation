4b96de9a8a5c6f12a4e01e4cccf79007f8716d29
jaiganesh2108 <jaig7335@gmail.com>
2026-08-14 22:40:57 +0530

    feat: python test

--- dummy.py
+++ dummy.py
@@ -1,5 +1,10 @@
-def area_of_rectangle(length, width):
-    return length * width
+my_str = input("Enter a string: ")
 
-rectangle_area = area_of_rectangle(5,10)
-print(f"the area of the rectangle is: {rectangle_area}")
+vowels = "aeiou"
+
+count = 0
+
+for i in my_str:
+    if i in vowels:
+        count += 1
+print(count)
