e4481613f63d053a1d471fd5a431bf46674cb471
jaiganesh2108 <jaig7335@gmail.com>
2026-08-15 23:10:48 +0530

    feat: reversing string

--- dummy.py
+++ dummy.py
@@ -1,12 +1,2 @@
-s = "    I love python programming!    "
-print(s.upper())
-print(s.lower())
-print(s.strip())
-print(s.replace("python", "Quantum"))
-
-name = "jane"
-age = 25
-print(f"name: {name}, age: {age}")
-
-s = "My name is {} and I am {} years {}.".format( "Emily", 22, "old")
-print(s)
+s = "I love python programming"
+print(s[-3:0:-1])
