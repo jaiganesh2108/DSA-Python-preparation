bfe0b824a0e4f9b129f7c44edb1f288dfc91ba24
jaiganesh2108 <jaig7335@gmail.com>
2026-08-15 22:53:28 +0530

    feat : string operations and executions

--- dummy.py
+++ dummy.py
@@ -1,5 +1,9 @@
-s = "ABCDEF"
-print(s[1:4])    
-print(s[:3])     
-print(s[3:])    
-print(s[::-1])
+s = "    I love python programming!    "
+print(s.upper())
+print(s.lower())
+print(s.strip())
+print(s.replace("python", "Quantum"))
+
+name = "jane"
+age = 25
+print(f"name: {name}, age: {age}")
