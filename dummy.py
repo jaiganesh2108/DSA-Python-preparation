2c19a0c3213ceadd86f3fe3d6d1e275799fad975
jaiganesh2108 <jaig7335@gmail.com>
2026-08-18 23:25:38 +0530

    feat:roman number finder

--- dummy.py
+++ dummy.py
@@ -1,2 +1,19 @@
-s = set("GeeksForGeeks")
-print(s)
+values = {
+    'I': 1,
+    'V': 5, 
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+s = input().upper()
+result = 0
+
+for i in range(len(s)):
+    if i+1 < len(s) and values[s[i]] < values[s[i+1]]:
+        result -= values[s[i]]
+    else:
+        result += values[s[i]]
+
+print(result)
