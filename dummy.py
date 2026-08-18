9a0788f86c33ec75032444c3a9fef6f005985b04
jaiganesh2108 <jaig7335@gmail.com>
2026-08-18 21:22:32 +0530

    feat: update

--- dummy.py
+++ dummy.py
@@ -1,12 +1,2 @@
-dict = {"name": "jai", "age": 21}
-print(dict)
-
-dict["name"] = "jg"
-print(dict)
-
-list = [1,2,3,4]
-list[2] = 10
-print(list)
-
-dict["age"] = 10
-print(dict)
+s = set("GeeksForGeeks")
+print(s)
